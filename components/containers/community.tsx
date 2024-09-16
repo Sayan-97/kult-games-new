f7756afeeb972252d0931b8f2a36931915245cd7
Sayan-97 <sdey4232@gmail.com>
2024-09-16 21:14:25 +0530

    sections updated

--- components/containers/community.tsx
+++ components/containers/community.tsx
@@ -3,6 +3,8 @@ import DiscordImg from "@/public/imgs/discord-image.png";
 import TelegramImg from "@/public/imgs/telegram-icon.png";
 import InstagramImg from "@/public/imgs/instagram-icon.png";
 import AnnouncementImg from "@/public/imgs/announcement-icon.png";
+import TwitterImg from "@/public/imgs/twitter-img-2.png";
+import Button from "../shared/button";
 
 export default function OurCommunity() {
   return (
@@ -12,8 +14,8 @@ export default function OurCommunity() {
         <div className="w-full grid lg:grid-cols-2 gap-6">
           <div className="space-y-6">
             <div className="bg-[radial-gradient(172.27%_145.85%_at_-4.55%_-1.7%,rgba(89,106,197,0.30)0%,rgba(89,106,197,0.00)100%)] flex gap-4 rounded-2xl overflow-hidden">
-              <div className="p-4">
-                <h4>Discord Community</h4>
+              <div className="p-4 space-y-4">
+                <h5 className="text-[27px] font-bold">Discord Community</h5>
                 <p>Explore an ultimate marketplace designed for gamers.</p>
               </div>
               <Image
@@ -37,6 +39,19 @@ export default function OurCommunity() {
                 Announcement
               </div>
             </div>
+          </div>
+
+          <div className="rounded-2xl max-lg:space-y-4 bg-[radial-gradient(172.27%_145.85%_at_-4.55%_-1.7%,rgba(89,106,197,0.30)0%,rgba(89,106,197,0.00)100%)] p-4 flex flex-col justify-between">
+            <div className="flex flex-col lg:flex-row items-start lg:items-end max-lg:gap-4 justify-between">
+              <div className="lg:w-2/3 space-y-5">
+                <h5 className="text-[27px] font-bold">Follow us on Twitter</h5>
+                <p>
+                  Follow us on twitter to get latest updates and announcements
+                </p>
+              </div>
+              <Button>Follow Now</Button>
+            </div>
+            <Image src={TwitterImg} alt="img" className="w-full" />
           </div>
         </div>
       </div>
