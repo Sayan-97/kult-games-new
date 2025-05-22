dd3d52de3de199f913b68d12bcff38aca1c5d4da
rishabhjaiswal3 <rj838486@gmail.com>
2025-05-22 21:39:55 +0530

    added changes

--- components/containers/bulletstorm-banner.tsx
+++ components/containers/bulletstorm-banner.tsx
@@ -2,20 +2,26 @@ import React from "react";
 import Image from "next/image";
 import Button from "../shared/button";
 import Link from "next/link";
-import XtremeBg from "@/public/imgs/xtremestuntbg.png";
+// import XtremeBg from "@/public/imgs/xtremestuntbg.png";
+// import XtremeLogo from "@/public/imgs/xtremelogo.png";
+
+import rageRoadsBg from '@/public/imgs/xtreme-new-image.png';
+import roadRagesLogo from '@/public/imgs/rage-roads.png';
+import sophandLogo from '@/public/imgs/Sophon_Logo_Main.png';
+
 import XtremeBgSm from "@/public/imgs/xtremestuntbg-SM.png";
-import XtremeLogo from "@/public/imgs/xtremelogo.png";
+
 
 export default function BulletStormBanner() {
   return (
     <section className="container">
-      <div className="relative rounded-2xl overflow-hidden bg-[#04080C]">
+      <div className="relative rounded-2xl overflow-hidden bg-[#04080C] min-h-[540px] flex flex-col justify-end">
         <Image
-          src={XtremeBg}
+          src={rageRoadsBg}
           alt="img"
           // fill
           priority
-          className="bottom-0 w-[60%] right-0 max-md:hidden brightness-75 absolute"
+          className="bottom-0 w-[100%] h-[100%] right-0 max-md:hidden brightness-75 absolute"
         />
         <Image
           src={XtremeBgSm}
@@ -23,23 +29,21 @@ export default function BulletStormBanner() {
           priority
           className="absolute bottom-0 md:hidden"
         />
-        <div className="relative flex flex-col justify-between gap-2 lg:gap-8 p-8 max-md:pb-32">
-          <Image src={XtremeLogo} alt="img" className="w-52 lg:w-96" />
-          <p className="lg:w-[40%] text-xl lg:text-4xl leading-normal lg:leading-normal capitalize font-generalSans">
-            🏆 Unlock true glory with the Exclusive Pass
-          </p>
+        <div className="top-14 relative flex flex-row justify-between gap-2 lg:gap-8 p-8 max-md:pb-32">
+          <Image src={roadRagesLogo} alt="img" className="w-52 lg:w-40" />
           <div className="flex items-center gap-8">
-            <Link href="https://sooshpad.xyz/project-detail/xtreme-stunt">
+            <Link href="https://mintify.xyz/launchpad/sophonriderspass">
               <Button className="w-32 lg:w-40 h-11">
                 <p className="text-xs lg:text-sm">Mint NFT</p>
               </Button>
             </Link>
-            {/* <Link href="https://discord.gg/Z5mkgXHs">
+            {/* <Link href="https://mintify.xyz/launchpad/sophonriderspass">
               <Button className="w-32 lg:w-40 h-11">
                 <p className="text-xs lg:text-sm">Join Discord</p>
               </Button>
             </Link> */}
           </div>
+          <Image src={sophandLogo} alt="img" className="w-52 lg:w-40" />
         </div>
       </div>
     </section>
--- components/shared/game-popup.tsx
+++ components/shared/game-popup.tsx
@@ -8,6 +8,10 @@ import PopupImg from "@/public/imgs/xtremestuntbg.png";
 import Button from "./button";
 import Link from "next/link";
 import XtremeLogo from "@/public/imgs/xtremelogo.png"
+import rageRoadsBg from '@/public/imgs/xtreme-new-image.png';
+import roadRagesLogo from '@/public/imgs/rage-roads.png';
+import sophandLogo from '@/public/imgs/Sophon_Logo_Main.png';
+
 
 export default function GamePopup() {
   const [open, setOpen] = useState(false);
@@ -20,28 +24,59 @@ export default function GamePopup() {
   }, []); // Empty dependency array to run only once
   return (
     <Dialog open={open} onOpenChange={setOpen}>
-      <DialogContent className="pt-14 max-w-[48rem] bg-background border-none bg-[#04080C]">
-        <Image
-          src={PopupImg}
-          alt="img"
-          priority
-          className="block max-md:pt-20"
-        />
-
-        <div className="absolute top-16 left-10 space-y-4">
-        <Image src={XtremeLogo} alt="img" className="w-32 lg:w-52" />
-          <p className="lg:w-[80%] text-xl lg:text-2xl leading-normal lg:leading-normal capitalize font-generalSans">
-          🏆 Unlock true glory with the Exclusive Pass
-          </p>
-          <div className="flex items-center gap-8">
-            <Link href="https://sooshpad.xyz/project-detail/xtreme-stunt">
-              <Button className="w-24 lg:w-32 h-9 outline-none">
-                <p className="text-xs lg:text-sm">Mint NFT</p>
-              </Button>
-            </Link>
+      <DialogContent className="pt-0 max-w-[48rem] border-none flex flex-col items-center justify-center bg-transparent shadow-none [&>button.absolute]:hidden">
+        <div className="relative w-full flex justify-center max-w-[90%]">
+          <Image
+            src={rageRoadsBg}
+            alt="img"
+            priority
+            className="block max-w-[48rem] rounded-xl"
+          />
+          <button
+            onClick={() => setOpen(false)}
+            className="absolute top-1 right-1 w-10 h-10 flex items-center justify-center text-xl font-bold z-20 rounded-full focus:outline-none focus:ring-0 focus-visible:outline-none"
+            aria-label="Close"
+          >
+            ×
+          </button>
+          <div className="w-full flex flex-row justify-between items-center gap-4 z-10 absolute bottom-0 left-0 ">
+            <Image src={roadRagesLogo} alt="img" className="w-22 lg:w-32 " />
+            <div className="flex items-center gap-8">
+              <Link href="https://mintify.xyz/launchpad/sophonriderspass">
+                <Button className="w-24 lg:w-32 h-9 outline-none">
+                  <p className="text-xs lg:text-sm">Mint NFT</p>
+                </Button>
+              </Link>
+            </div>
+            <Image src={sophandLogo} alt="img" className="w-22 lg:w-32" />
           </div>
         </div>
       </DialogContent>
     </Dialog>
+    // <Dialog open={open} onOpenChange={setOpen}>
+    //   <DialogContent className="pt-14 max-w-[48rem] bg-background border-none bg-[#04080C]">
+    //     <Image
+    //       src={rageRoadsBg}
+    //       alt="img"
+    //       priority
+    //       className="block max-md:pt-20 max-w-[48rem]"
+    //     />
+
+    //     <div className="absolute top-16 left-10 space-y-4">
+    //     <Image src={XtremeLogo} alt="img" className="w-32 lg:w-52" />
+    //       <p className="lg:w-[80%] text-xl lg:text-2xl leading-normal lg:leading-normal capitalize font-generalSans">
+    //       🏆 Unlock true glory with the Exclusive Pass
+    //       </p>
+    //       <div className="flex items-center gap-8">
+            
+    //         <Link href="https://mintify.xyz/launchpad/sophonriderspass">
+    //           <Button className="w-24 lg:w-32 h-9 outline-none">
+    //             <p className="text-xs lg:text-sm">Mint NFT</p>
+    //           </Button>
+    //         </Link>
+    //       </div>
+    //     </div>
+    //   </DialogContent>
+    // </Dialog>
   );
 }
