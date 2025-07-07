ac8d8b0c029e49709601a1ecb074a32ced4a4f2f
Suryamani Gupta <manigupta28201@gmail.com>
2025-07-07 14:05:22 +0530

    Fix: Adjust popup size and x-button alignment; remove 'Start Mint' button on Hero page

--- components/shared/game-popup.tsx
+++ components/shared/game-popup.tsx
@@ -9,11 +9,10 @@ import Button from "./button";
 import Link from "next/link";
 // import XtremeLogo from "@/public/imgs/xtremelogo.png"
 // import rageRoadsBg from '@/public/imgs/xtreme-new-image.png';
-import bg from '@/public/imgs/bg.png';
-import kultLogo from '@/public/imgs/kult-games-logo.png';
+import bg from "@/public/imgs/bg.png";
+import kultLogo from "@/public/imgs/kult-games-logo.png";
 // import roadRagesLogo from '@/public/imgs/rage-roads.png';
 // import sophandLogo from '@/public/imgs/Sophon_Logo_Main.png';
-
 
 export default function GamePopup() {
   const [open, setOpen] = useState(false);
@@ -27,12 +26,12 @@ export default function GamePopup() {
   return (
     <Dialog open={open} onOpenChange={setOpen}>
       <DialogContent className="pt-0 max-w-[48rem] border-none flex flex-col items-center justify-center bg-transparent shadow-none [&>button.absolute]:hidden">
-        <div className="relative w-full flex justify-center max-w-[99%]">
+        <div className="relative w-full flex justify-center max-w-[80%]">
           <Image
             src={bg}
             alt="img"
             priority
-            className="block lg:max-w-[48rem] sm:max-w-[42rem] rounded-xl min-h-[200px] "
+            className="block lg:max-w-[36rem] sm:max-w-[42rem] rounded-xl min-h-[200px] "
           />
 
           <button
@@ -42,24 +41,23 @@ export default function GamePopup() {
           >
             ×
           </button>
-          <div className="w-full flex lg:pb-8 flex-row justify-around items-center gap-4 z-10 absolute bottom-0 left-0 ">            
-            <Image src={kultLogo} alt="img" className="w-16 lg:w-32" />
+          <div className="w-full flex lg:pb-8 flex-row justify-center items-center gap-4 z-10 absolute bottom-0 left-0 ">
+            <Image src={kultLogo} alt="img" className="w-8 lg:w-16" />
             <div className="flex items-center gap-8">
               <Link href="https://highwayhustle.xyz/">
                 <Button className="w-33 lg:w-34 h-9 outline-none">
-                  <p className="text-xs lg:text-sm color-red-302">Start Game</p>
+                  <p className="text-xs lg:text-sm color-red-302">Play Games</p>
                 </Button>
               </Link>
             </div>
-            <div className="flex items-center gap-8">
+            {/* <div className="flex items-center gap-8">
               <Link href="https://app.mintify.com/launchpad/morphspeedlegends">
                 <Button className="w-33 lg:w-34 h-9 outline-none">
                   <p className="text-xs lg:text-sm color-red-302">Mint NFT</p>
                 </Button>
               </Link>
-            </div>
+            </div> */}
           </div>
-      
         </div>
       </DialogContent>
     </Dialog>
