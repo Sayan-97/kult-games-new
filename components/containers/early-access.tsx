0c6271df5b26ffc7e0c6561c47fba94b8b4932f2
Rahul Gangwar <rahulgangwar4567@gmail.com>
2025-12-25 00:22:35 +0530

    animation added responsive for tablet and mobile

--- components/containers/early-access.tsx
+++ components/containers/early-access.tsx
@@ -11,11 +11,11 @@ export default function EarlyAccess() {
       <div className="container">
         <AnimatedSection variant={scaleIn} threshold={0.3} className="relative">
           <Image src={BgImg} alt="img" className="max-md:hidden" priority draggable={false} />
-          <div className="md:absolute inset-0 flex flex-col items-center justify-center text-center gap-8">
-            <h3 className="capitalize text-[32px] font-ethnocentric">
+          <div className="md:absolute inset-0 flex flex-col items-center justify-center text-center gap-4 md:gap-5 lg:gap-8 md:px-8 lg:px-0">
+            <h3 className="capitalize text-[28px] md:text-[26px] lg:text-[32px] font-ethnocentric">
               Join the early access
             </h3>
-            <p className="font-semibold text-[15px]">
+            <p className="font-semibold text-[15px] md:text-[14px] lg:text-[15px] md:max-w-[70%] lg:max-w-none">
               Join the waitlist to access all our games early with exciting
               rewards and benefits
             </p>
